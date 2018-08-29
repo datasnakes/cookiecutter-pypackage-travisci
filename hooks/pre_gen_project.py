0f5f8453c137371a3eab84224cb32635d41c6842
Shaurita D. Hutchins <ms.s.hutchins@gmail.com>
2018-08-29 17:32:34 -0500

    Use a regex to prevent invalid module names

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -1,2 +1,13 @@
 # -*- coding: utf-8 -*-
+import re
+import sys
 
+MODULE_REGEX = r'^[_a-zA-Z][_a-zA-Z0-9]+$'
+
+module_name = '{{cookiecutter.python_package_name}}'
+
+if not re.match(MODULE_REGEX, module_name):
+    print('ERROR: The project slug (%s) is not a valid Python project/module name. Please do not use a - and use _ instead.' % module_name)
+
+    # Exit to cancel project creation
+    sys.exit(1)
